7868ed94af8bf362aabf96d96d320dab8294bb9a
Martin Moreira <172557112+mimoreirac@users.noreply.github.com>
2025-02-13 20:56:44 -0500

    Añade form para crear productos

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,5 @@ app_name = 'store'
 urlpatterns = [
     path("", views.index, name="index"),
     path("products/", views.all_products, name="all_products"),
+    path("add_product/", views.add_product, name="add_product"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import ListView, DetailView
-from .models import MusicProduct, ElectronicProduct
 from itertools import chain
+
+from .models import MusicProduct, ElectronicProduct
+from store.forms import ProductTypeForm, MusicProductForm, ElectronicProductForm
+
+
 
 def index(request):
     music_products = MusicProduct.objects.all()[:3]
@@ -28,3 +32,25 @@ def all_products(request):
         'products': combined_products,
     }
     return render(request, 'products.html', context)
+
+def add_product(request):
+    type_form = ProductTypeForm(request.POST or None)
+    music_form = MusicProductForm(request.POST or None, request.FILES or None)
+    electronic_form = ElectronicProductForm(request.POST or None, request.FILES or None)
+    
+    if request.method == 'POST' and 'product_type' in request.POST:
+        if request.POST['product_type'] == 'music' and music_form.is_valid():
+            music_form.save()
+            messages.success(request, "Producto añadido exitosamente.")
+            return redirect('music_products_list')
+        elif request.POST['product_type'] == 'electronic' and electronic_form.is_valid():
+            electronic_form.save()
+            messages.success(request, "Producto añadido exitosamente.")
+            return redirect('electronic_products_list')
+    
+    context = {
+        'type_form': type_form,
+        'music_form': music_form,
+        'electronic_form': electronic_form,
+    }
+    return render(request, 'add_product.html', context)
